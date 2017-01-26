dfbedb21ee69e8115520d5fb8de1a89ab1078203
Luis Fernando Barrera <luisfernando@informind.com>
2017-01-26 01:23:59 -0600

    Procesamiento de datos

--- scripts/procesardatos.py
+++ scripts/procesardatos.py
@@ -1,10 +1,13 @@
 import os
 import glob
 import tablib
+import datetime
 import pymongo
 
 TYPES = {'txt', 'png', 'wmv', 'rtf', 'pps', 'jpg', 'wma', 'mdi', 'doc', 'pptx', 'pdf', 'xls', 'htm', 'docx', 'gif'}
-
+TEXT_TYPES = ['txt', 'rtf', 'doc', 'pdf', 'htm', 'docx']
+DATA_TYPES = ['xls']
+MEDIA_TYPES = ['png', 'wmv', 'pps', 'jpg', 'gif', 'wma', 'mdi', 'pptx']
 
 data = tablib.Dataset(headers=[])
 
@@ -12,13 +15,15 @@ archivos = [file for file in glob.glob("articledb/**/*", recursive=True) if os.p
 nombre_archivo = ["".join(archivo.split('/')[-1].split('.')[0:-1]) for archivo in archivos]
 extensiones = [archivo.split('.')[-1].lower() for archivo in archivos]
 carpetas = ["/".join(archivo.split('/')[1:-1]) for archivo in archivos]
+created = [datetime.datetime.fromtimestamp(os.stat(archivo).st_mtime) for archivo in archivos]
 
 data.append_col(nombre_archivo, header='filename')
 data.append_col(extensiones, header='ext')
 data.append_col(carpetas, header='folder')
 data.append_col(archivos, header='path')
+data.append_col(archivos, header='created')
 
-data.headers = ['filename', 'ext', 'folder', 'path']
+data.headers = ['filename', 'ext', 'folder', 'path', 'created']
 
 
 def write_to_csv(data):
