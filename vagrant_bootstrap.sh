83ea0b2b59b19c906108b3c3f71e9aa8d7d5a44a
Luis Fernando Barrera <luisfernando@informind.com>
2017-02-23 13:35:11 -0600

    Vagrant Script

--- vagrant_bootstrap.sh
+++ vagrant_bootstrap.sh
@@ -3,6 +3,8 @@
 
 # Install git for version control, pip for install python packages
 echo 'Installing git, Python 3, and pip...'
+sudo apt-get update
+sudo apt-get upgrade
 sudo apt-get install -y language-pack-es libfreetype6-dev
 #ziblcms2-dev libwebp-dev tcl8.6-dev tk8.6-dev python-tk
 sudo apt-get -qq install build-essential git python3 python3-dev libjpeg-dev libtiff5-dev zlib1g-dev postgresql-9.5 libpq5 libpq-dev > /dev/null 2>&1
