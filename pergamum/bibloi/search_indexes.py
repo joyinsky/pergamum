81110d2a262a84170bbad81324b0d75611e36170
Luis Fernando Barrera <luisfernando@informind.com>
2017-03-02 02:28:35 -0600

    Índices de Búsqueda

--- pergamum/bibloi/search_indexes.py
+++ pergamum/bibloi/search_indexes.py
@@ -5,6 +5,7 @@ from .models import Article
 
 class ArticleIndex(indexes.SearchIndex, indexes.Indexable):
     text = indexes.CharField(document=True)
+    name = indexes.CharField(model_attr='name')
     author = indexes.CharField(model_attr='author')
     date = indexes.CharField(model_attr='date')
 
@@ -14,3 +15,11 @@ class ArticleIndex(indexes.SearchIndex, indexes.Indexable):
     def index_queryset(self, using=None):
         """Used when the entire index for model is updated."""
         return self.get_model().objects.filter(updated_at__lte=datetime.datetime.now())
+
+    def prepare_author(self, obj):
+        author = u''
+
+        if 'author' in self.prepared_data:
+            author = self.prepared_data['author']
+
+        return author
