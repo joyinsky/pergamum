a79d10d4a9bac93f9b9914005a25c3ece17a9b22
Luis Fernando Barrera <luisfernando@informind.com>
2017-03-14 14:22:07 -0600

    Folder Browse

--- pergamum/bibloi/views.py
+++ pergamum/bibloi/views.py
@@ -1,7 +1,7 @@
 from django.urls import reverse_lazy
 from vanilla import ListView, CreateView, DetailView, UpdateView, DeleteView
 from .forms import ArticleForm, ArticleSearchForm
-from .models import Article
+from .models import Article, Folder
 from haystack.generic_views import SearchView
 from haystack.query import SearchQuerySet
 
@@ -34,3 +34,19 @@ class ArticleSearch(SearchView):
     template_name = 'search/search.html'
     form_class = ArticleSearchForm
     queryset = SearchQuerySet().order_by('-date')
+
+
+class FolderView(ListView):
+    model = Folder
+    template_name = 'bibloi/folder_browse.html'
+
+    def get_queryset(self):
+        return self.model.objects.filter()
+
+    def get_context_data(self, **kwargs):
+        context = super(FolderView, self).get_context_data(**kwargs)
+        print(self.kwargs)
+        context['current_folder'] = '/' + self.kwargs.get('path')
+        context['folders'] = ['Folder A', 'Folder B', 'Folder C']
+        context['files'] = ['File 1', 'File 2', 'File 3']
+        return context
